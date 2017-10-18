6c23b69b446123a6da0a5cfef8174c90db43aca6
Drunut <drunut@gmail.com>
2017-10-18 16:33:36 -0600

    Edited Links

--- view/new-cat.php
+++ view/new-cat.php
@@ -2,15 +2,15 @@
 <html lang="en-us">
     <head>
         <title>Acme</title>
-        <?php include "common/head.php" ?>
+        <?php include "../common/head.php" ?>
     </head>
 
     <body>
       <div id="floatFrame">
         <header>
-            <img id="logo" src="images/site/logo.gif" alt="Acme Inc. Logo">
+            <img id="logo" src="../images/site/logo.gif" alt="Acme Inc. Logo">
             <div id="headerAccountDiv">
-                <img id="myAccountImg" src="images/site/account.gif" alt="My Account">
+                <img id="myAccountImg" src="../images/site/account.gif" alt="My Account">
                 <p>My Account</p>
             </div>
             <nav>
@@ -22,11 +22,11 @@
         </header>
 
         <main id="indexMain">
-            <h1>Create New Category</h1>
+            <h1>New Category</h1>
         </main>
 
         <footer>
-            <?php include "common/footer.php"?>
+            <?php include "../common/footer.php"?>
         </footer>
       </div>
     </body>
