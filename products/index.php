8c96b6497b939bce296c2017e37d17ec8299f83f
Drunut <drunut@gmail.com>
2017-10-25 19:28:30 -0600

    Ancillary changes to be in line with the Accounts Controller

--- products/index.php
+++ products/index.php
@@ -48,23 +48,62 @@ switch ($action) {
 	case 'newProduct':
             include "../view/new-prod.php";
             break;
+        
         // Form Submission Cases
         case 'createProduct':
-            try {
-                //There are a TON of keys here so it's cleaner to filter the whole POST array, then extract
-                //$formAction = array_filter($_POST, trim());
-                $formAction = filter_input_array(INPUT_POST);
-                extract($formAction);
-		addProduct( $catListDropDown, $productName, $productDescription, $productImage, $productThumbnail, $productPrice, $productStock, $productSize, $productWeight, $productLocation, $productStyle, $productVendor );
-		$message = "<h2 id='message'>Congratulations, ".$productName." was successfully added.</h2>";
-            } catch (Exception $prodEx) {
-                $message = "<h2 id='message'> Submission Error</h2>";
+            //There are a TON of keys here so it's cleaner to filter the whole POST array
+            $formAction = filter_input_array(INPUT_POST);
+            // Extract the array into variables
+            extract($formAction);
+            // Check the array if there are any empty fields.
+            // We extract before this so we can replace filled-in fields
+            foreach($formAction as $formItem){
+                if(empty($formItem)){
+                    $message = "<h2 id='message'>Please provide information for all empty form fields.</h2>";
+                    include '../view/new-prod.php';
+                    exit;
+                }
+            }
+//             empty($catListDropDown) || empty($productName) || empty($productDescription) ||
+//             empty($productImage) || empty($productThumbnail) || empty($productPrice) ||
+//             empty($productStock) || empty($productSize) || empty($productWeight) ||
+//             empty($productLocation) || empty($productStyle) || empty($productVendor)
+            // Send Product Data to the Model
+            $prodOutcome =  addProduct( $catListDropDown, $productName, $productDescription
+                                      , $productImage, $productThumbnail, $productPrice
+                                      , $productStock, $productSize, $productWeight
+                                      , $productLocation, $productStyle, $productVendor );
+            // Make sure it changed just one row (the one we added)
+            if($prodOutcome === 1) {
+                    $message = "<h2 id='message'>Congratulations, ".$productName." was successfully added.</h2>";
+                    include '../view/new-prod.php';
+                    exit;
+                } else {
+                    $message = "<h2 id='message'>".$productName." registration failed. Please try again.</h2>";
+                    include '../view/new-prod.php';
+                    exit;
+                }
+            break;
+            
+        case 'createCategory':
+            //Check if there's a supplied category name
+            if (empty($categoryName)){
+                $message = "<h2 id='message'>Please provide a category name.</h2>";
+                include '../view/new-cat.php';
+                exit;
+            }
+            // Insert the new category into the database
+            $catOutcome = addCategory( filter_input(INPUT_POST, 'categoryName') );
+            // Check to see if the insert was successful
+            if ($catOutcome === 1) {
+                header( "Location: /acme/products/index.php" );
+            } else {
+                $message = "<h2 id='message'>".$categoryName." registration failed. Please try again.</h2>";
+                include '../view/new-cat.php';
+                exit;
             }
             break;
-        case 'createCategory':
-            addCategory( filter_input(INPUT_POST, 'categoryName') );
-            header( "Location: /acme/products/index.php" );
-            break;
+            
 	default:
             include "../view/prod-mgmt.php";
 }
