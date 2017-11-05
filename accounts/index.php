019d9d61695ad3896b8c0301c015a3a8d8c661ce
Drunut <tekulve.nikolaas@gmail.com>
2017-11-04 21:26:45 -0600

    Implemented Login Sessions

--- accounts/index.php
+++ accounts/index.php
@@ -71,16 +71,40 @@ switch ($action) {
                 }
                 break;
         case 'submitLogin':
-            $clientEmail = filter_input(INPUT_POST, 'clientEmail');
-            $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_STRING);
+            $clientEmail = filter_input(INPUT_POST, 'clientEmail', FILTER_SANITIZE_EMAIL);
             $clientEmail = checkEmail($clientEmail);
-            $checkPassword = checkPassword($clientPassword); // 1/0 == T/F
+            $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_STRING);
+            $passwordCheck = checkPassword($clientPassword); // 1/0 == T/F
             if(empty($clientEmail) || empty($checkPassword)) {
                     $message = "<h2 id='message'>Please provide information for all empty form fields.</h2>";
                     include '../view/login.php';
                     exit;
                 }
-            break;
+            // Continue Login
+            // 
+            // Query the client data based on the email address
+            $clientData = getClient($clientEmail);
+            // Compare the password just submitted against
+            // the hashed password for the matching client
+            $hashCheck = password_verify($clientPassword, $clientData['clientPassword']);
+            // If the hashes don't match create an error
+            // and return to the login view
+            if (!$hashCheck) {
+              $message = '<p class="notice">Please check your password and try again.</p>';
+              include '../view/login.php';
+              exit;
+            }
+            // A valid user exists, log them in
+            $_SESSION['loggedin'] = TRUE;
+            // Remove the password from the array
+            // the array_pop function removes the last
+            // element from an array
+            array_pop($clientData);
+            // Store the array into the session
+            $_SESSION['clientData'] = $clientData;
+            // Send them to the admin view
+            include '../view/admin.php';
+            exit;
 	default:
 		include "../view/500.php";
 }
