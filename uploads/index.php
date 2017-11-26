a0de031bfb016b436de7ca8915b0972eab09360d
Drunut <tekulve.nikolaas@gmail.com>
2017-11-25 21:16:07 -0700

    Created image upload functionality, thumbnail generation, and a page to manage it all on

---
+++ uploads/index.php
@@ -0,0 +1,134 @@
+<?php
+/* 
+ * The Image Uploads Controller
+ */
+
+session_start(); // access session
+
+// Various outside resources
+require_once '../library/connections.php';
+require_once '../model/acme-model.php';
+require_once '../model/products-model.php';
+require_once '../model/uploads-model.php'; //This one is new
+require_once '../library/functions.php';
+
+//Action filtering
+$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+}
+
+// build header account to show logout button
+if(isset($_SESSION['loggedin'])){
+    $headerAccount = createHeaderAccount(true);
+} else {
+    $headerAccount = createHeaderAccount(false);
+}
+
+//Build our nav
+$categories = getCategories();
+$navList = createNav($categories);
+
+////////////////////////////////////////////////////////////////////////////////
+
+/* * ****************************************************
+* Variables for use with the Image Upload Functionality
+* **************************************************** */
+// directory name where uploaded images are stored
+$image_dir = '/acme/uploads/images';
+// The path is the full path from the server root
+$image_dir_path = $_SERVER['DOCUMENT_ROOT'] . $image_dir;
+
+
+// Our switch functions
+switch ($action) {
+    
+    case 'upload':
+        // Store the incoming product id
+        $invId = filter_input(INPUT_POST, 'invId', FILTER_VALIDATE_INT);
+        // Store the name of the uploaded image
+        $imgName = $_FILES['file1']['name'];
+
+        $imageCheck = checkExistingImage($imgName);
+
+        if($imageCheck){
+            $message = "<h2 class='message'>An image by that name already exists.</h2>";
+        } elseif (empty($invId) || empty($imgName)) {
+            $message = "<h2 class='message'>You must select a product and image file for the product.</p>";
+        } else {
+
+            // Upload the image, store the returned path to the file
+            $imgPath = uploadFile('file1');
+
+            // Insert the image information to the database, get the result
+            $result = storeImages($imgPath, $invId, $imgName);
+
+            // Set a message based on the insert result
+            if ($result) {
+                $message = '<h2 class="message">The upload succeeded.</h2>';
+            } else {
+                $message = '<h2 class="message">Sorry, the upload failed.</h2>';
+            }
+        }
+
+        // Store message to session
+        $_SESSION['message'] = $message;
+
+        // Redirect to this controller for default action
+        header('location: .');
+    break;
+
+    
+    case 'delete':
+        // Get the image name and id
+        $filename = filter_input(INPUT_GET, 'filename', FILTER_SANITIZE_STRING);
+        $imgId = filter_input(INPUT_GET, 'imgId', FILTER_VALIDATE_INT);
+
+        // Build the full path to the image to be deleted
+        $target = $image_dir_path . '/' . $filename;
+
+        // Check that the file exists in that location
+        if (file_exists($target)) {
+            // Deletes the file in the folder
+            $result = unlink($target); 
+        }
+
+        // Remove from database only if physical file deleted
+        if ($result) {
+            $remove = deleteImage($imgId);
+        }
+
+        // Set a message based on the delete result
+        if ($remove) {
+            $message = "<h2 class='message'>$filename was successfully deleted.</h2>";
+        } else {
+            $message = "<h2 class='message'>$filename was NOT deleted.</h2>";
+        }
+
+        // Store message to session
+        $_SESSION['message'] = $message;
+
+        // Redirect to this controller for default action
+        header('location: .');
+    break;
+
+    
+    default:
+        // Call function to return image info from database
+        $imageArray = getImages();
+
+        // Build the image information into HTML for display
+        if (count($imageArray)) {
+            $imageDisplay = buildImageDisplay($imageArray);
+        } else {
+            $imageDisplay = '<h2 class="message">Sorry, no images could be found.</h2>';
+        }
+
+        // Get inventory information from database
+        $products = getProductBasics();
+        // Build a select list of product information for the view
+        $prodSelect = buildProductsSelect($products);
+
+        include '../view/image-admin.php';
+    break;
+}
---
+++ view/image-admin.php
@@ -0,0 +1,71 @@
+<?php 
+    if (isset($_SESSION['message'])) {
+        $message = $_SESSION['message'];
+    }
+    
+    
+    
+    
+    
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <title>Image Management</title>
+        <?php include "../common/head.php" ?>
+    </head>
+
+    <body>
+      <div id="floatFrame">
+        <header>
+            <img id="logo" src="../images/site/logo.gif" alt="Acme Inc. Logo">
+            <?php
+                  echo $headerAccount
+            ?>
+            <nav>
+                <?php
+                    // include "common/nav.php"
+                    echo $navList;
+                ?>
+            </nav>
+        </header>
+
+        <main>
+            
+            
+            <h1>Image Management</h1>
+            <p>Welcome to the image Management Page! Please select an option below.</p>
+            
+            <section id="newProductImage">
+                <h2>Add New Product Image</h2>
+                <?php if (isset($message)) { echo $message; } ?>
+                <form action="/acme/uploads/" method="post" enctype="multipart/form-data">
+                    <label for="invItem">Product</label><br>
+                    <?php echo $prodSelect; ?><br><br>
+                    <label>Upload Image:</label><br>
+                    <input type="file" name="file1"><br>
+                    <input type="submit" class="regbtn" value="Upload">
+
+                    <input type="hidden" name="action" value="upload">
+                </form>
+            </section>
+            
+            <hr>
+            
+            <section id="existingProductImage">
+                <h2>Existing Images</h2>
+                <p class="message">If deleting an image, delete the thumbnail too and vice versa.</p>
+                <?php if (isset($imageDisplay)) { echo $imageDisplay; } ?>
+            </section>
+        </main>
+
+        <footer>
+            <?php include "../common/footer.php"?>
+        </footer>
+      </div>
+    </body>
+
+
+
+</html>
+<?php unset($_SESSION['message']); ?>
