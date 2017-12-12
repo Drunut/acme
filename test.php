398bffeb97f5c88b4ae31e568ff0c75bd3d4075d
Drunut <tekulve.nikolaas@gmail.com>
2017-12-11 18:04:24 -0700

    Task3::Created new Specific Product page on the Test Page

--- test.php
+++ test.php
@@ -23,19 +23,53 @@
         </header>
         <main id="spPage">
             <img id="spImage" src="/acme/images/products/rubberband.jpg" alt="Monster Rubber Band Product Image">
-            <h1 id="spName" class="spStats">                Monster Rubber Band </h1>
-            <p id="spVendor" class="spStats">               By: Rubbermaid </p>
-            <p id="spPrice" class="spAvailability">         $4.00 </p>
-            <p id="spStock" class="spAvailability">         4589 in stock </p>
-            <p id="spLocation" class="spAvailability">      Ships from:<br>Cedar Point, IO </p>
-            <p id="spStyle" class="spStats">                Rubber<span>style</span> </p>
-            <p id="spWeight" class="spStats">               1 lbs. /<span id="spSize" class="spStats">75 ft<sup>3</sup></span> </p>
-            <p id="spDescription" class="spExtended">       These are not tiny rubber bands. These are MONSTERS!
-                                                            These bands can stop a train locamotive or be used as a slingshot for cows.
-                                                            Only the best materials are used!
-            </p>
-            <p id="spReviews" class="spExtended">           I like chocolate Milk </p>
-            <img class="spThumbs" src="/acme/images/products/rubberband-tn.jpg" alt="rubberband-tn.jpg">            
+            <section id="spThumbs">
+                <img src="/acme/images/products/rubberband-tn.jpg" alt="rubberband-tn.jpg">
+                <img src="/acme/images/products/sweatPig-tn.jpg" alt="sweatPig-tn.jpg">
+                <img src="/acme/images/products/oh_my-tn.jpg" alt="oh_my-tn.jpg">
+            </section>
+            
+            <h1 id="spName">                Monster Rubber Band </h1>
+            <section id="spStats">
+                <p id="spVendor">               By: Rubbermaid </p>
+                <div>
+                    <p id="spStyle">                Rubber<span>style</span> </p>
+                    <p id="spWeight">               1 lbs. /<span id="spSize" class="spStats">75 ft<sup>3</sup></span> </p>
+                </div>
+            </section>
+            
+            <section id="spAvailability">
+                <p id="spPrice">         $4.00 </p>
+                <p id="spStock">         4589 in stock </p>
+                <p id="spLocation">      Ships from:<br>Cedar Point, IO </p>
+            </section>
+            
+            <section id="spMessage">
+                <p>Reviews can be found at the bottom of the page</p>
+            </section>
+            
+            <section id="spExtended">
+                <p id="spDescription">       These are not tiny rubber bands. These are MONSTERS!
+                                                                These bands can stop a train locamotive or be used as a slingshot for cows.
+                                                                Only the best materials are used!
+                </p>
+                <section id="spReviewSection">
+                    <h2>Customer Reviews</h2>
+                    <sub>Review the Monster Rubber Band</sub>
+                    <article class="spReview">
+                        <h3 class="author">TSawyer</h3>
+                        <p class="timestamp">wrote on 10 March, 2017:</p>
+                        <p class="comment">This is wonderful. You can catch an elephant with this thing!</p>
+                    </article>
+                    <article class="spReview">
+                        <h3 class="author">TSawyer</h3>
+                        <p class="timestamp">wrote on 10 March, 2017:</p>
+                        <p class="comment">This is wonderful. You can catch an elephant with this thing!</p>
+                    </article>
+                </section>
+                
+            </section>
+            
             
         </main>
 
